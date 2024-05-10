d4a9939de7d356d31380ad520843bbebcc1568a2
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2024-05-10 16:24:14 +0530

    using chi middlewares instead of negroni

--- server/server.go
+++ server/server.go
@@ -5,23 +5,31 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/jackc/pgx/v5"
-	"github.com/urfave/negroni"
 )
 
 func NewServer(logger *slog.Logger, db *pgx.Conn) http.Handler {
 	mux := chi.NewMux()
-
+	mux.Use(middleware.RequestID)
+	mux.Use(middleware.RealIP)
+	mux.Use(middleware.Logger)
+	mux.Use(middleware.Recoverer)
 	addRoutes(mux, logger, db)
 
-	n := negroni.Classic()
-	n.UseHandler(mux)
-
-	return n
+	return mux
 }
 
 type ServiceError struct {
 	Message string `json:"message"`
 	Code    int    `json:"code"`
 	Extra   any    `json:"extra"`
+}
+
+type PaginatedResponse[T any] struct {
+	Data     []T `json:"data"`
+	Page     int `json:"page"`
+	Count    int `json:"count"`
+	Pages    int `json:"pages"`
+	PageSize int `json:"page_size"`
 }
